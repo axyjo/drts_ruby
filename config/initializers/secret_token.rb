8db1f0c6a2683b2e9f23d40fd6df6c9721a50d1e
Akshay Joshi <joshi.a@gmail.com>
2011-05-17 05:47:51 -0400

    Rails 3.1 default stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DrtsRuby::Application.config.secret_token = '6f4de67895604724e7fc7dd7de44b328fb8a5d372d37fe8698b65dbfd44e487077023f4e83515d527d729c49985ac4274a52e7eb8fde95c3062f9ae09548e144'
