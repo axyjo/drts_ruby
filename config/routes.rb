0380e8be03c5e90af496ed57b2ac096e2ce59466
Akshay Joshi <joshi.a@gmail.com>
2011-05-21 01:59:14 -0400

    Added the users controller and model for registration.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 DrtsRuby::Application.routes.draw do
   get "users/new"
+  get "sign_up" => "users#new", :as => "sign_up"
+  root :to => "users#new"
+  resources :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
