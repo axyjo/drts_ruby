02aa3022f69494fc09f8adba71bc9f3b70f7d78e
Akshay Joshi <joshi.a@gmail.com>
2011-05-22 08:13:11 -0400

    Don't commit the generated Javascript files.

--- public/javascripts/application.js
+++
@@ -1,9 +0,0 @@
-/* DO NOT MODIFY. This file was compiled Sun, 22 May 2011 11:52:21 GMT from
- * /home/akshay/Dropbox/Webdev/drts_ruby/app/scripts/application.coffee
- */
-
-(function() {
-  $(document).ready(function() {
-    return Map.init();
-  });
-}).call(this);
--- public/javascripts/game.js
+++
@@ -1,8 +0,0 @@
-/* DO NOT MODIFY. This file was compiled Sun, 22 May 2011 11:52:21 GMT from
- * /home/akshay/Dropbox/Webdev/drts_ruby/app/scripts/game.coffee
- */
-
-(function() {
-  var Game;
-  Game = Game || {};
-}).call(this);
