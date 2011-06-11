64b28833ef470bacba774be9188de829c2708647
Akshay Joshi <joshi.a@gmail.com>
2011-06-11 10:53:23 -0400

    Added tile preloading at the default zoom level.

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,5 +1,6 @@
 class MapsController < ApplicationController
   def view
+    @initial_tiles = get_initial_tiles
   end
 
   def click
@@ -15,7 +16,7 @@ class MapsController < ApplicationController
     tiles.each do |tile|
       tile_params = tile.split('-')
       if tile_params.count == 4
-        type, x, y, z = tile_params
+        layer, x, y, z = tile_params
         if Float(x) and Float(y) and Float(z)
           x = x.to_i
           y = y.to_i
@@ -23,8 +24,8 @@ class MapsController < ApplicationController
           # max tiles = map size / 2^z
           max_tiles = 512 / 2**z
           if x >= 0 and y >= 0 and z >= 0 and x < max_tiles and y < max_tiles and z <= 7
-            html = generate_tile_html(x, y, z)
-            @json_tiles.push({'id' => tile, 'type' => type, 'html' => html})
+            html = generate_tile_html(layer, x, y, z)
+            @json_tiles.push({'id' => tile, 'type' => layer, 'html' => html})
           end
         end
       end
@@ -46,12 +47,27 @@ class MapsController < ApplicationController
     end
   end
 
-  def generate_tile_html(x, y, z)
+  def generate_tile_html(layer, x, y, z)
     tile_size = 128
     left = (x*tile_size).to_s
     top = (y*tile_size).to_s
     tile_type = get_tile_type(x, y, z)
-    html = '<div class="map_tiles tiles-sprite tiles-'+tile_type+'" id="'+tile+'" style="left:'+left+'px; top:'+top+'px;"></div>'
+    id = "#{layer}-#{x}-#{y}-#{z}"
+    html = '<div class="map_tiles tiles-sprite tiles-'+tile_type+'" id="'+id+'" style="left:'+left+'px; top:'+top+'px;"></div>'
+    return html
+  end
+
+  def get_initial_tiles
+    default_z = 5
+    max_tiles = 512 / 2**default_z
+    map_size = 512
+    html = ""
+    for x in 0..max_tiles
+      for y in 0..max_tiles
+        temp = generate_tile_html('base', x, y, default_z)
+        html = "#{html}#{temp}\n"
+      end
+    end
     return html
   end
 end
