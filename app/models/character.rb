5bdc7495d2cc8e0269712cb8fd768d92d5ba1649
Akshay Joshi <joshi.a@gmail.com>
2011-06-12 16:18:17 -0400

    Add the relationship between characters and rounds.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,4 @@
 class Character < ActiveRecord::Base
   belongs_to :user
+  belongs_to :round
 end
--- app/models/round.rb
+++ app/models/round.rb
@@ -1,2 +1,3 @@
 class Round < ActiveRecord::Base
+  has_many :characters, :dependent => :destroy
 end
