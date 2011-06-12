7994c1f775b358d6437fc3600353a2224acdd1b0
Akshay Joshi <joshi.a@gmail.com>
2011-06-12 16:25:29 -0400

    Added the relationship between cities and kingdoms.

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,2 +1,3 @@
 class City < ActiveRecord::Base
+  has_one :kingdom
 end
--- app/models/kingdom.rb
+++ app/models/kingdom.rb
@@ -1,3 +1,5 @@
 class Kingdom < ActiveRecord::Base
   belongs_to :character
+  belongs_to :capital, :class_name => "City", :conditions => { :capital => true }
+  has_many :cities
 end
