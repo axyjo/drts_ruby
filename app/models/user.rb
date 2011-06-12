2e16dccfcd6d9e251222e9d7335ae189c1f11cdc
Akshay Joshi <joshi.a@gmail.com>
2011-06-12 16:17:13 -0400

    Add the relationship between characters and users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :characters, :dependent => :destroy
+
   attr_accessible :email, :password, :password_confirmation
   attr_accessor :password
   before_save :encrypt_password
