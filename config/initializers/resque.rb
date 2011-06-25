e4f988ee6721eb80937662aaacd55827d8754e5d
Akshay Joshi <joshi.a@gmail.com>
2011-06-25 13:59:49 -0400

    Add the tile cache warmer task upon loading the application.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -4,3 +4,13 @@ ENV["REDIS_URL"] ||= "redis://game:l8SxAPzcDXQgy0KfMiUj@efbb8c00.dotcloud.com:91
 uri = URI.parse(ENV["REDIS_URL"])
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
 
+maxZoom = 4
+for z in 0..maxZoom
+  maxTilesX = 2**(z+2) - 1
+  maxTilesY = 2**(z+2) - 1
+  for x in 0..maxTilesX
+    for y in 0..maxTilesY
+      Resque.enqueue(TileCacher, 'base/' + z + '/' + x + '/' + y)
+    end
+  end
+end
