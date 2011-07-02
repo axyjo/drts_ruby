4ab302d20e3b2be23e814d28cecc07c46ff03d1f
Akshay Joshi <joshi.a@gmail.com>
2011-07-01 21:27:35 -0400

    Convert user migration to Rails 3.1.

--- db/migrate/20110520140431_create_users.rb
+++ db/migrate/20110520140431_create_users.rb
@@ -1,15 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
-  def self.up
+  def change
     create_table :users do |t|
       t.string :username
       t.string :email
       t.string :password_digest
-
       t.timestamps
     end
-  end
-
-  def self.down
-    drop_table :users
   end
 end
