1271913ccd64349b3bffebe3ffef9bd4df40502c
Akshay Joshi <joshi.a@gmail.com>
2011-07-09 18:04:21 -0400

    Remove non-Bayeux handling from Faye. GH #20.

--- faye/server.js
+++ faye/server.js
@@ -2,17 +2,11 @@ var http = require('http'),
     faye = require('./faye');
 
 var bayeux = new faye.NodeAdapter({
-    mount:    '/faye',
+    mount:    '/',
     timeout:  45
 });
 
-// Handle non-Bayeux requests
-var server = http.createServer(function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('Nothing to see here, move along! Path: ');
-  response.write(request.url);
-  response.end();
-});
+var server = http.createServer();
 
 bayeux.attach(server);
 server.listen(8080);
