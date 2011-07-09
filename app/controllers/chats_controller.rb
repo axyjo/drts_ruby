6703b658453bae6ec65edd3ad8d1e5e2cbfe5ce2
Akshay Joshi <joshi.a@gmail.com>
2011-07-09 17:40:28 -0400

    Converted the chat function to use the broadcast function. GH #18.

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,12 +1,6 @@
 class ChatsController < ApplicationController
   def new
-    puts current_user.username
-    puts params[:msg]
-    client = Faye::Client.new('http://localhost:9292/faye')
-    require 'eventmachine'
-    EM.run {
-      client.publish('/global_chat', {'user' => current_user.username, 'msg' => params[:msg]})
-    }
+    broadcast '/global_chat', {'user' => current_user.username, 'msg' => params[:msg]}
     render :text => 'success'
   end
 end
