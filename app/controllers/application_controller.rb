badeaff7e4dfc88c7b0ed5a24e0a6a5131c84f47
Akshay Joshi <joshi.a@gmail.com>
2011-07-09 17:38:08 -0400

    Added the broadcast method along with the Net::HTTP dependency. GH #18.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user
 
+  def broadcast(channel, data)
+    message = {:channel => channel, :data => data}
+    uri = URI.parse("http://localhost:9292/faye")
+    Net::HTTP.post_form(uri, :message => message.to_json)
+  end
+
   private
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
