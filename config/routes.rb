2c2c9342039eb5399fc2f6690a911727ae381659
Akshay Joshi <joshi.a@gmail.com>
2011-07-09 17:27:13 -0400

    Added server-side support for chat. GH #13.

--- config/routes.rb
+++ config/routes.rb
@@ -59,6 +59,8 @@ DrtsRuby::Application.routes.draw do
   get "maps/view"
   match "/tiles/:type/:z/:x/:y" => "maps#tiles", :as => :tiles
 
+  match "/chat/send" => "chats#new"
+
   get "log_in" => "sessions#new", :as => "log_in"
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "sign_up" => "users#new", :as => "sign_up"
