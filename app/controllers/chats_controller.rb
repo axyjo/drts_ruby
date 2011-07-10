08be533fb65187e0871de866bb5287b48e303882
Akshay Joshi <joshi.a@gmail.com>
2011-07-09 23:30:22 -0400

    Sanitize inputs for chat. GH #13.

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,7 +1,8 @@
 class ChatsController < ApplicationController
   def new
     if params[:msg] != ''
-      broadcast '/global_chat', {'user' => current_user.username, 'msg' => params[:msg]}
+      message = ActionController::Base.helpers.sanitize params[:msg]
+      broadcast '/global_chat', {'user' => current_user.username, 'msg' => message}
       render
     end
   end
