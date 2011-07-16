35b9719b9e8306ab90a01cff5b0c03d18a95d39f
Akshay Joshi <joshi.a@gmail.com>
2011-07-16 17:42:31 -0400

    Added resque_scheduler require statements. GH #51.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,4 +1,8 @@
 if Rails.env.production?
+  require 'resque'
+  require 'resque_scheduler'
+  require 'resque/scheduler'
+
   uri = URI.parse(REDIS_URL)
   Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
 
