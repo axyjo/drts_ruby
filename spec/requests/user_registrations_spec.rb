faf1bb2185bb3b3348b4d263f778ca2ac4985673
Akshay Joshi <joshi.a@gmail.com>
2011-07-21 18:05:59 -0400

    Check if the registration form works on valid data and rejects on invalid data. GH #63.

--- spec/requests/user_registrations_spec.rb
+++ spec/requests/user_registrations_spec.rb
@@ -7,7 +7,33 @@ describe "UserRegistrations" do
   end
 
   it "allows users fill the form" do
+    visit sign_up_path
+    fill_in "Username", :with => "test"
+    fill_in "Email", :with => "test@example.com"
+    fill_in "Password", :with => "password"
+    fill_in "Password confirmation", :with => "password"
+    click_button "Create account"
+    current_path.should eq(root_path)
+  end
 
+  it "checks for invalid form data" do
+    visit sign_up_path
+    fill_in "Username", :with => "test"
+    click_button "Create account"
+    page.should have_content("Email can't be blank")
+    page.should have_content("Password can't be blank")
+
+    visit sign_up_path
+    fill_in "Email", :with => "test"
+    click_button "Create account"
+    page.should have_content("Username can't be blank")
+    page.should have_content("Password can't be blank")
+
+    visit sign_up_path
+    fill_in "Password", :with => "password"
+    fill_in "Password confirmation", :with => "password2"
+    click_button "Create account"
+    page.should have_content("Password doesn't match")
   end
 
   it "sends users an email after registering" do
