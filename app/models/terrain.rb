da3d696292eae5522efccd7ac54c1fac769eac6c
Akshay Joshi <joshi.a@gmail.com>
2011-07-24 01:53:07 -0400

    Added validators for terrain. GH #64.

--- app/models/terrain.rb
+++ app/models/terrain.rb
@@ -1,3 +1,6 @@
 class Terrain < ActiveRecord::Base
   attr_accessible :name, :blue_value
+
+  validates_presence_of :name
+  validates_numericality_of :blue_value, :only_integer => true, :greater_than => -1, :less_than => 256
 end
