8634e862d57fb3345447b97dffb99c9c74dc69cd
Akshay Joshi <joshi.a@gmail.com>
2011-07-24 01:29:25 -0400

    Added initial terrain values. GH #64.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Terrains:
+Terrain.create(name: "Plains", blue_value: 0)
+Terrain.create(name: "Forest", blue_value: 1)
+Terrain.create(name: "Hills", blue_value: 2)
+Terrain.create(name: "Mountains", blue_value: 3)
+Terrain.create(name: "Impassable Mountains", blue_value: 4)
+Terrain.create(name: "Desert", blue_value: 5)
+Terrain.create(name: "Tundra", blue_value: 6)
+Terrain.create(name: "Swamp", blue_value: 7)
+Terrain.create(name: "River", blue_value: 8)
+Terrain.create(name: "Ocean/Sea", blue_value: 255)
