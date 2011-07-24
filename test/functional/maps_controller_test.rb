ec30f50abb57146ca48edd1a6a67043b1843efca
Akshay Joshi <joshi.a@gmail.com>
2011-07-24 01:18:54 -0400

    Removed the old test files. GH #58.

--- test/functional/maps_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class MapsControllerTest < ActionController::TestCase
-  test "should get view" do
-    get :view
-    assert_response :success
-  end
-
-end
--- test/unit/province_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ProvinceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
