0f1891aea1c6d767359fc466d116490ff559b5f3
Akshay Joshi <joshi.a@gmail.com>
2011-07-24 22:16:25 -0400

    Added values for terrain costs from the wiki. GH #75.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 # Terrains:
-Terrain.create(name: "Plains", blue_value: 0)
-Terrain.create(name: "Forest", blue_value: 1)
-Terrain.create(name: "Hills", blue_value: 2)
-Terrain.create(name: "Mountains", blue_value: 3)
-Terrain.create(name: "Impassable Mountains", blue_value: 4)
-Terrain.create(name: "Desert", blue_value: 5)
-Terrain.create(name: "Tundra", blue_value: 6)
-Terrain.create(name: "Swamp", blue_value: 7)
-Terrain.create(name: "River", blue_value: 8)
-Terrain.create(name: "Ocean/Sea", blue_value: 255)
+Terrain.create(name: "Plains", blue_value: 0, cost: 1)
+Terrain.create(name: "Forest", blue_value: 1, cost: 1.5)
+Terrain.create(name: "Hills", blue_value: 2, cost: 5)
+Terrain.create(name: "Mountains", blue_value: 3, cost: 7)
+Terrain.create(name: "Impassable Mountains", blue_value: 4, cost: -1)
+Terrain.create(name: "Desert", blue_value: 5, cost: -1)
+Terrain.create(name: "Tundra", blue_value: 6, cost: -1)
+Terrain.create(name: "Swamp", blue_value: 7, cost: -1)
+Terrain.create(name: "River", blue_value: 8, cost: -1)
+Terrain.create(name: "Ocean/Sea", blue_value: 255, cost: -1)
 
 # Coordinates:
 for x in 1..Rails.configuration.game[:gameSize]
