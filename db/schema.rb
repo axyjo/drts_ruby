2aa8a4bfb78b6ad678c1045ba0e68b7b2804777e
Akshay Joshi <joshi.a@gmail.com>
2011-07-24 21:16:28 -0400

    Updated schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110724044312) do
+ActiveRecord::Schema.define(:version => 20110725010841) do
 
   create_table "coordinates", :force => true do |t|
     t.integer  "lat"
     t.integer  "lng"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "province_id"
+    t.integer  "terrain_id"
   end
 
   create_table "empires", :force => true do |t|
