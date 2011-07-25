8ab251dfefa0f8eda587287a134fb08c1d589eae
Akshay Joshi <joshi.a@gmail.com>
2011-07-24 23:56:53 -0400

    Add three separate test cases for neighbours. GH #76.

--- spec/models/coordinate_spec.rb
+++ spec/models/coordinate_spec.rb
@@ -1,5 +1,43 @@
 require 'spec_helper'
 
 describe Coordinate do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "generates the correct distance between two points" do
+
+  end
+
+  it "generates the right neighbours when both coordinates are on the edge" do
+    coord1 = Coordinate.lnglat(1, 1)
+    coord1.north.lng.should eq(1)
+    coord1.north.lat.should eq(128)
+    coord1.south.lng.should eq(1)
+    coord1.south.lat.should eq(2)
+    coord1.west.lng.should eq(128)
+    coord1.west.lat.should eq(1)
+    coord1.east.lng.should eq(2)
+    coord1.east.lat.should eq(1)
+  end
+
+  it "generates the right neighbours when one coordinate is on the edge" do
+    coord2 = Coordinate.lnglat(128, 121)
+    coord2.north.lng.should eq(128)
+    coord2.north.lat.should eq(120)
+    coord2.south.lng.should eq(128)
+    coord2.south.lat.should eq(122)
+    coord2.west.lng.should eq(127)
+    coord2.west.lat.should eq(121)
+    coord2.east.lng.should eq(1)
+    coord2.east.lat.should eq(121)
+  end
+
+  it "generates the right neighbours when no coordinates are on the edge" do
+    coord3 = Coordinate.lnglat(64, 64)
+    coord3.north.lng.should eq(64)
+    coord3.north.lat.should eq(63)
+    coord3.south.lng.should eq(64)
+    coord3.south.lat.should eq(65)
+    coord3.west.lng.should eq(63)
+    coord3.west.lat.should eq(64)
+    coord3.east.lng.should eq(65)
+    coord3.east.lat.should eq(64)
+  end
 end
