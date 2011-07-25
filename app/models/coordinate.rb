51cfb45189816a60a3cf51f16cb0e98a6339d2de
Akshay Joshi <joshi.a@gmail.com>
2011-07-24 23:41:26 -0400

    Added a better getter and basic implementations of the cardinal directions. GH #76.

--- app/models/coordinate.rb
+++ app/models/coordinate.rb
@@ -1,4 +1,24 @@
 class Coordinate < ActiveRecord::Base
   belongs_to :province
   has_one :terrain
+
+  def lnglat(lng, lat)
+    Coordinate.find_by_lng_and_lat(lng, lat)
+  end
+
+  def north
+    Coordinate.lnglat(lng, lat-1)
+  end
+
+  def south
+    Coordinate.lnglat(lng, lat+1)
+  end
+
+  def west
+    Coordinate.lnglat(lng-1, lat)
+  end
+
+  def east
+    Coordinate.lnglat(lng+1, lat)
+  end
 end
