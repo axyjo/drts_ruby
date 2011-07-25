64341ffa1a87a58cc0e1ec3a4d66cb766ce8635c
Akshay Joshi <joshi.a@gmail.com>
2011-07-24 22:14:18 -0400

    Add the cost column to terrains. GH #75.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110725010841) do
+ActiveRecord::Schema.define(:version => 20110725021309) do
 
   create_table "coordinates", :force => true do |t|
     t.integer  "lat"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(:version => 20110725010841) do
     t.integer  "blue_value"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.float    "cost"
   end
 
   create_table "users", :force => true do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,3 @@
-# Coordinates:
-for x in 1..Rails.configuration.game[:gameSize]
-  # Use a transaction to speed things up.
-  ActiveRecord::Base.transaction do
-    for y in 1..Rails.configuration.game[:gameSize]
-      Coordinate.create(lat: x, lng: y)
-    end
-  end
-end
-
 # Terrains:
 Terrain.create(name: "Plains", blue_value: 0)
 Terrain.create(name: "Forest", blue_value: 1)
@@ -19,3 +9,13 @@ Terrain.create(name: "Tundra", blue_value: 6)
 Terrain.create(name: "Swamp", blue_value: 7)
 Terrain.create(name: "River", blue_value: 8)
 Terrain.create(name: "Ocean/Sea", blue_value: 255)
+
+# Coordinates:
+for x in 1..Rails.configuration.game[:gameSize]
+  # Use a transaction to speed things up.
+  ActiveRecord::Base.transaction do
+    for y in 1..Rails.configuration.game[:gameSize]
+      Coordinate.create(lat: x, lng: y)
+    end
+  end
+end
