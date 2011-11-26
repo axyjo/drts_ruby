803d827e8a03e3a54f5959fdce909af44e63d909
Akshay Joshi <joshi.a@gmail.com>
2011-11-26 04:23:48 -0500

    Add the math behind calculating the tiles. GH #101.

--- node/server.js
+++ node/server.js
@@ -8,17 +8,49 @@ var bayeux = new faye.NodeAdapter({
 });
 
 var server = http.createServer(function(req, res) {
-    var segments = require('url').parse(req.url).pathname;
-    segments = segments.split('/').splice(1, 4);
+  var segments = require('url').parse(req.url).pathname;
+  segments = segments.split('/');
 
+  if(segments.length == 5) {
+    var maxZoom = 3;
+    var sliceSize = 2048;
+    // We're dealing with tiles.
     try {
-      var dir = fs.lstatSync(segments[0]);
-      res.WriteHead(200, {'Content-Type': 'image/png'});
-      res.end('Hello World\n');
+      var type = segments[1];
+      var z    = parseInt(segments[2]);
+      var x    = parseInt(segments[3]);
+      var y    = parseInt(segments[4]);
+
+      // Check if directory exists.
+      var dir = fs.lstatSync(type);
+
+      if(z >= 0 && z <= maxZoom) {
+        var scale = Math.pow(2, z-maxZoom);
+        // Chunk count is the number of chunks in the tile at the current zoom.
+        var chunkCount = Math.pow(2, z);
+
+        // Calculate which pre-cut tile will contain what we want.
+        var tileY = Math.floor(x/chunkCount);
+        var tileY = Math.floor(y/chunkCount);
+
+        // Calculate how much of the pre-cut tile we want.
+        var chunkWidth = sliceSize/chunkCount;
+        var chunkHeight = sliceSize/chunkCount;
+        var chunkX = (x % chunkCount) * chunkWidth;
+        var chunkY = (y % chunkCount) * chunkHeight;
+
+        res.writeHead(200, {'Content-Type': 'image/png'});
+        res.end('Hello World\n');
+      } else {
+        throw new Error('Zoom-level is out of bounds.');
+      }
     } catch (e) {
-      res.WriteHead(200, {'Content-Type': 'text/plain'});
-      res.end(e);
+      res.writeHead(500, {'Content-Type': 'text/plain'});
+      res.end(e.message);
     }
+  }
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end("Leave me alone!");
 });
 
 bayeux.attach(server);
