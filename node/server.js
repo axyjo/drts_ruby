f29adfdf89df5a35b05d3f9e74625de447853cac
Akshay Joshi <joshi.a@gmail.com>
2011-11-26 03:53:02 -0500

    Add check for directory in the node tile server. GH #101.

--- node/server.js
+++ node/server.js
@@ -1,4 +1,5 @@
 var http = require('http'),
+    fs   = require('fs'),
     faye = require('./faye');
 
 var bayeux = new faye.NodeAdapter({
@@ -6,7 +7,19 @@ var bayeux = new faye.NodeAdapter({
     timeout:  45
 });
 
-var server = http.createServer();
+var server = http.createServer(function(req, res) {
+    var segments = require('url').parse(req.url).pathname;
+    segments = segments.split('/').splice(1, 4);
+
+    try {
+      var dir = fs.lstatSync(segments[0]);
+      res.WriteHead(200, {'Content-Type': 'image/png'});
+      res.end('Hello World\n');
+    } catch (e) {
+      res.WriteHead(200, {'Content-Type': 'text/plain'});
+      res.end(e);
+    }
+});
 
 bayeux.attach(server);
 server.listen(8080);
