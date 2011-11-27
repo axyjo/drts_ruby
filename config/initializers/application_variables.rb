bbc288a8ce94686d4856564204b276fb33a2c66a
Akshay Joshi <joshi.a@gmail.com>
2011-11-27 13:58:28 -0500

    Fix for the Faye path.

--- config/initializers/application_variables.rb
+++ config/initializers/application_variables.rb
@@ -1,6 +1,6 @@
 if File.exists?("/home/dotcloud/environment.json")
   app_vars = JSON.parse(File.read("/home/dotcloud/environment.json"))
-  FAYE_URL = "http://game-axyjo.dotcloud.com/faye/"
+  FAYE_URL = "http://webapp-axyjo.dotcloud.com/faye/"
 else
   FAYE_URL = "http://localhost:9292/faye/"
 end
