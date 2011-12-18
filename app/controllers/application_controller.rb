c64cfa94ffd114b5a7586bc7b6440570ecfaf148
Akshay Joshi <joshi.a@gmail.com>
2011-12-18 18:24:12 -0500

    Remove the old current_user helper method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  helper_method :current_user
   before_filter :check_empire
   before_filter :check_path
 
@@ -17,10 +16,6 @@ class ApplicationController < ActionController::Base
   end
 
   private
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
   def check_empire
     if current_user
       if current_user.empire.nil?
