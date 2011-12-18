1e5b197d3cde347f6fa57c8f9d4482f646d58d97
Akshay Joshi <joshi.a@gmail.com>
2011-12-18 00:26:53 -0500

    Add performance metrics for tile cache hits/misses.

--- node/cache.js
+++ node/cache.js
@@ -1,6 +1,12 @@
 var sys = require('sys');
+var fs = require('fs');
+var db = require('db-mysql');
+
+var env = JSON.parse(fs.readFileSync('/home/dotcloud/environment.json', 'utf-8'));
 
 var cache = {}
+var hit_count = 0
+var miss_count = 0
 function now() {
   return (new Date).getTime();
 }
@@ -8,7 +14,7 @@ function now() {
 exports.put = function(key, value, ttl) {
   var expire = now() + ttl;
   cache[key] = {value: value, expire: expire}
-
+  miss_count++;
   if(!isNaN(expire)) {
     setTimeout(function() {
       exports.del(key);
@@ -25,6 +31,7 @@ exports.get = function(key) {
   if(typeof data != "undefined") {
     if(isNaN(data.expire) || data.expire >= now()) {
       return data.value;
+      hit_count++;
     } else {
       exports.del(key);
     }
@@ -43,3 +50,30 @@ exports.getTTL = function(key) {
   }
   return null;
 }
+
+setInterval(function() {
+  var tot = hit_count + miss_count;
+  if(tot == 0) {
+    tot = 1
+  }
+  db.Database({
+    hostname: env["DOTCLOUD_DB_MYSQL_HOST"],
+    port: env["DOTCLOUD_DB_MYSQL_PORT"],
+    user: env["DOTCLOUD_DB_MYSQL_LOGIN"],
+    password: env["DOTCLOUD_DB_MYSQL_PASSWORD"],
+    database: 'game_production'
+  }).connect(function(err) {
+    var h = hit_rate/tot*100;
+    var m = miss_rate/tot*100;
+    this.query().insert('performance_metrics',
+      ['timestamp', 'metric', 'description', 'value'],
+      [{value: 'NOW', escape: false}, 'tile_cache_hit_rate', 'in percent', h]
+    ).insert('performance_metrics',
+      ['timestamp', 'metric', 'description', 'value'],
+      [{value: 'NOW', escape: false}, 'tile_cache_miss_rate', 'in percent', m]
+    ).execute(function(err, result) {
+      hit_count = 0;
+      miss_count = 0;
+    });
+  });
+}, 5*60*1000);
