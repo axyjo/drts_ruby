53f746673b084aef9c19f8d3286e36fc89e6640e
Akshay Joshi <joshi.a@gmail.com>
2011-12-19 15:02:59 -0500

    Use sessions in database instead of cookies.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-DrtsRuby::Application.config.session_store :cookie_store, key: '_drts_ruby_session'
+#DrtsRuby::Application.config.session_store :cookie_store, key: '_drts_ruby_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# DrtsRuby::Application.config.session_store :active_record_store
+DrtsRuby::Application.config.session_store :active_record_store
