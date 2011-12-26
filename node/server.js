0b6655a8878ecd532b6fc0bfb0fa7446d0541305
Akshay Joshi <joshi.a@gmail.com>
2011-12-26 01:30:44 -0500

    Add more zoom levels. This commit may be unstable.

--- node/server.js
+++ node/server.js
@@ -1,6 +1,7 @@
 var http = require('http'),
     fs   = require('fs'),
-    cache= require('./cache');
+    path = require('path'),
+    cache= require('./cache'),
     im   = require('imagemagick'),
     faye = require('faye');
 
@@ -25,8 +26,9 @@ var server = http.createServer(function(req, res) {
       // Check if directory exists.
       var dir = fs.lstatSync(type);
 
-      var maxZoom = 3;
+      var maxZoom = 7;
       var sliceSize = 2048;
+      var tileSize = 256;
       if(z >= 0 && z <= maxZoom) {
         if(val = cache.get(tileID)) {
           res.writeHead(200, {
@@ -37,41 +39,80 @@ var server = http.createServer(function(req, res) {
         }
         var TTL = cache.getTTL(tileID);
         if(isNaN(TTL) || TTL < 60*1000 || !val) {
-          var scale = Math.pow(2, z-maxZoom);
+          var scale = Math.pow(2, z-(Math.log(sliceSize/tileSize)/Math.log(2));
 
           // The number of chunks in the tile at the current zoom.
           var chunkCount = Math.pow(2, z);
+          if(scale >= 1) {
+            // Calculate which pre-cut tile will contain what we want.
+            var tileX = Math.floor(x/chunkCount).toString();
+            var tileY = Math.floor(y/chunkCount).toString();
 
-          // Calculate which pre-cut tile will contain what we want.
-          var tileX = Math.floor(x/chunkCount).toString();
-          var tileY = Math.floor(y/chunkCount).toString();
+            // Calculate how much of the pre-cut tile we want.
+            var chunkWidth = sliceSize/chunkCount;
+            var chunkHeight = sliceSize/chunkCount;
+            var chunkX = (x % chunkCount) * chunkWidth;
+            var chunkY = (y % chunkCount) * chunkHeight;
 
-          // Calculate how much of the pre-cut tile we want.
-          var chunkWidth = sliceSize/chunkCount;
-          var chunkHeight = sliceSize/chunkCount;
-          var chunkX = (x % chunkCount) * chunkWidth;
-          var chunkY = (y % chunkCount) * chunkHeight;
+            var imgPath = path.join(type, tileX, tileY) + '.png';
+            path.exists(imgPath, function(exists) {
+              if(exists) {
+                var crop = chunkWidth +'x'+ chunkHeight +'+'+ chunkX +'+'+ chunkY;
+                var op = [imgPath, '-crop', crop, '+repage', '-scale', '256x256', '-'];
+                im.convert(op, function(err, stdout) {
+                  if(err) throw err;
+                  cache.put(tileID, stdout, 5*60*1000);
+                  if(isNaN(TTL) || !val) {
+                    res.writeHead(200, {
+                      'Content-Type': 'image/png',
+                      'Content-Length': stdout.length
+                    });
+                    res.end(stdout, 'binary');
+                  }
+                });
+              } else {
+                throw new Error('imgPath does not exist: '+imgPath);
+              }
+            });
+          } else {
+            var imagesToMerge = 1/scale;
+            var startX = x * chunkCount;
+            var startY = y * chunkCount;
+            var endX = startX + imagesToMerge;
+            var endY = startY + imagesToMerge;
 
-          var imgPath = require('path').join(type, tileX, tileY) + '.png';
-          require('path').exists(imgPath, function(exists) {
-            if(exists) {
-              var crop = chunkWidth +'x'+ chunkHeight +'+'+ chunkX +'+'+ chunkY;
-              var op = [imgPath, '-crop', crop, '+repage', '-scale', '256x256', '-'];
-              im.convert(op, function(err, stdout) {
-                if(err) throw err;
-                cache.put(tileID, stdout, 5*60*1000);
-                if(isNaN(TTL) || !val) {
-                  res.writeHead(200, {
-                    'Content-Type': 'image/png',
-                    'Content-Length': stdout.length
-                  });
-                  res.end(stdout, 'binary');
-                }
-              });
-            } else {
-              throw new Error('imgPath does not exist: '+imgPath);
-            }
-          });
+            var topLeftPath = path.join(type, startX, startY) + '.png';
+            var bottomRightPath = path.join(type, endX, endY) + '.png';
+
+            path.exists(topLeftPath, function(exists) {
+              if(exists) {
+                path.exists(bottomRightPath, function(exists) {
+                  if(exists) {
+                    var op = [];
+                    for(var a = startY; a < endY; a++) {
+                      for(var b = startX; b < endX; b++) {
+                        op.push(path.join(type, b, a) + '.png');
+                      }
+                    }
+
+                    op.push('-mode', 'Concatenate', '-tile', '4x');
+                    op.push('+repage', '-scale', '256x256', '-');
+                    im.convert(op, function(err, stdout) {
+                      if(err) throw err;
+                      cache.put(tileID, stdout, 5*60*1000);
+                      if(isNaN(TTL) || !val) {
+                        res.writeHead(200, {
+                          'Content-Type': 'image/png',
+                          'Content-Length': stdout.length
+                        });
+                        res.end(stdout, 'binary');
+                      }
+                    });
+                  }
+                });
+              }
+            });
+          }
         }
       } else {
         throw new Error('Zoom-level is out of bounds.');
